53e5824f580fb820c0e2d9935f3726d773efbda7
Peter Dimov <pdimov@pdimov.com>
2017-06-24 04:09:41 +0300

    Update dependencies

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,5 +1,4 @@
 # Generated file. Do not edit.
 
-boost_declare_dependency(boost_config PUBLIC boost::config)
-boost_declare_dependency(boost_predef PUBLIC boost::predef)
-
+boost_declare_dependency(boost_config INTERFACE boost::config)
+boost_declare_dependency(boost_predef INTERFACE boost::predef)
