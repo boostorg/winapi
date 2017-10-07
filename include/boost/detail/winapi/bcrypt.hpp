d5adfec9d1ecff3e2c5e473b33b093f0e82e2679
Andrey Semashev <andrey.semashev@gmail.com>
2017-10-07 20:58:28 +0300

    Formatting and other nonessential changes.

--- include/boost/detail/winapi/bcrypt.hpp
+++ include/boost/detail/winapi/bcrypt.hpp
@@ -5,8 +5,8 @@
 //  Distributed under the Boost Software License, Version 1.0.
 //  See http://www.boost.org/LICENSE_1_0.txt
 
-#ifndef BOOST_DETAIL_WINAPI_BCRYPT_HPP
-#define BOOST_DETAIL_WINAPI_BCRYPT_HPP
+#ifndef BOOST_DETAIL_WINAPI_BCRYPT_HPP_
+#define BOOST_DETAIL_WINAPI_BCRYPT_HPP_
 
 #include <boost/detail/winapi/basic_types.hpp>
 
@@ -14,51 +14,60 @@
 #pragma once
 #endif
 
+#if defined(BOOST_WINAPI_IS_MINGW_W64) || (BOOST_USE_WINAPI_VERSION >= BOOST_WINAPI_VERSION_WIN6)
+
 #if BOOST_WINAPI_PARTITION_APP_SYSTEM
-#if defined(BOOST_WINAPI_IS_MINGW_W64) || (BOOST_USE_WINAPI_VERSION >= BOOST_WINAPI_VERSION_VISTA)
 
 #if defined(BOOST_USE_WINDOWS_H)
+
 #include <bcrypt.h>
-#else
 
 namespace boost { namespace detail { namespace winapi {
-    typedef PVOID_ BCRYPT_ALG_HANDLE_;
+typedef ::BCRYPT_ALG_HANDLE BCRYPT_ALG_HANDLE_;
+}}}
+
+#else // defined(BOOST_USE_WINDOWS_H)
+
+namespace boost { namespace detail { namespace winapi {
+typedef PVOID_ BCRYPT_ALG_HANDLE_;
 }}}
 
 extern "C" {
-boost::detail::winapi::NTSTATUS_ WINAPI
-    BCryptCloseAlgorithmProvider(
-        boost::detail::winapi::BCRYPT_ALG_HANDLE_ hAlgorithm,
-        boost::detail::winapi::ULONG_             dwFlags
-    );
 
 boost::detail::winapi::NTSTATUS_ WINAPI
-    BCryptGenRandom(
-        boost::detail::winapi::BCRYPT_ALG_HANDLE_ hAlgorithm,
-        boost::detail::winapi::PUCHAR_            pbBuffer,
-        boost::detail::winapi::ULONG_             cbBuffer,
-        boost::detail::winapi::ULONG_             dwFlags
-    );
+BCryptCloseAlgorithmProvider(
+    boost::detail::winapi::BCRYPT_ALG_HANDLE_ hAlgorithm,
+    boost::detail::winapi::ULONG_             dwFlags
+);
 
 boost::detail::winapi::NTSTATUS_ WINAPI
-    BCryptOpenAlgorithmProvider(
-        boost::detail::winapi::BCRYPT_ALG_HANDLE_ *phAlgorithm,
-        boost::detail::winapi::LPCWSTR_           pszAlgId,
-        boost::detail::winapi::LPCWSTR_           pszImplementation,
-        boost::detail::winapi::DWORD_             dwFlags
-    );
+BCryptGenRandom(
+    boost::detail::winapi::BCRYPT_ALG_HANDLE_ hAlgorithm,
+    boost::detail::winapi::PUCHAR_            pbBuffer,
+    boost::detail::winapi::ULONG_             cbBuffer,
+    boost::detail::winapi::ULONG_             dwFlags
+);
+
+boost::detail::winapi::NTSTATUS_ WINAPI
+BCryptOpenAlgorithmProvider(
+    boost::detail::winapi::BCRYPT_ALG_HANDLE_ *phAlgorithm,
+    boost::detail::winapi::LPCWSTR_           pszAlgId,
+    boost::detail::winapi::LPCWSTR_           pszImplementation,
+    boost::detail::winapi::DWORD_             dwFlags
+);
+
 } // extern "C"
-#endif // !BOOST_USE_WINDOWS_H
+
+#endif // defined(BOOST_USE_WINDOWS_H)
 
 namespace boost {
 namespace detail {
 namespace winapi {
 
 #if defined(BOOST_USE_WINDOWS_H)
-    typedef ::BCRYPT_ALG_HANDLE BCRYPT_ALG_HANDLE_;
-    const LPCWSTR_ BCRYPT_RNG_ALGORITHM_ = BCRYPT_RNG_ALGORITHM;
+const WCHAR_ BCRYPT_RNG_ALGORITHM_[] = BCRYPT_RNG_ALGORITHM;
 #else
-    const LPCWSTR_ BCRYPT_RNG_ALGORITHM_ = L"RNG";
+const WCHAR_ BCRYPT_RNG_ALGORITHM_[] = L"RNG";
 #endif
 
 using ::BCryptCloseAlgorithmProvider;
@@ -69,6 +78,8 @@ using ::BCryptOpenAlgorithmProvider;
 } // detail
 } // boost
 
-#endif // defined(BOOST_WINAPI_IS_MINGW_W64) || (BOOST_USE_WINAPI_VERSION >= BOOST_WINAPI_VERSION_VISTA)
 #endif // BOOST_WINAPI_PARTITION_APP_SYSTEM
-#endif // BOOST_DETAIL_WINAPI_BCRYPT_HPP
+
+#endif // defined(BOOST_WINAPI_IS_MINGW_W64) || (BOOST_USE_WINAPI_VERSION >= BOOST_WINAPI_VERSION_WIN6)
+
+#endif // BOOST_DETAIL_WINAPI_BCRYPT_HPP_
