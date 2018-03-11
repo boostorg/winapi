5e5a9c825281c91e9a543cf844dfbf9725280e76
Andrey Semashev <andrey.semashev@gmail.com>
2018-03-11 03:57:02 +0300

    Reverted addition of ntdef.h include as it causes macro clashes with winnt.h.

--- test/run/basic_types_abi.cpp
+++ test/run/basic_types_abi.cpp
@@ -14,9 +14,6 @@
 
 #include <boost/winapi/basic_types.hpp>
 #include <windows.h>
-#if BOOST_PLAT_WINDOWS_SDK_VERSION >= BOOST_WINAPI_WINDOWS_SDK_6_0
-#include <ntdef.h>
-#endif
 #include <boost/predef/platform/windows_uwp.h>
 #include "abi_test_tools.hpp"
 
